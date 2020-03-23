e033dc280fca29b530bc559c7ff07518346e97bf
jimohsodiq <58136523+jimohsodiq@users.noreply.github.com>
2020-03-23 14:07:48 -0700

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,12 @@
+var name = "Sodiq";
+console.log(name);
+
+var courses = ["HTML-CSS-JS", "Java", "Kotlin", "NodeJs", "CSharp", "Python", "Design"];
+console.log(courses);
+for (i = 1; i < 200; i++) {
+
+    console.log(i);
+    var odd = 1;
+
+    i = i + odd;
+}
